c24132cc42deed61941b630d12e7959e15cde7a9
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-06-27 16:09:16 +0700

    Up

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+from nltk import Tree
+from flask import Flask
+from flask import request
+from nltk.parse import CoreNLPParser
+
+app = Flask(__name__)
+
+parser = CoreNLPParser(url='http://localhost:9000')
+
+@app.route('/', methods = ['POST'])
+def extractByLabel(): 
+  text = request.form['text']
+  label = request.form['label']
+
+  tree = list(parser.raw_parse(text))
+  subtexts = []
+
+  for subtree in tree[0].subtrees():
+    if subtree.label()=="SBAR":
+      subtexts.append(' '.join(subtree.leaves()))
+
+  return json.dumps({
+    "subtexts": subtexts,
+    "fulltext": ' '.join(tree[0].leaves())
+  })
